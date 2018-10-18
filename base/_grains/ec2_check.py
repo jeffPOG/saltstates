f80c32f94f325daee882e72617f1004a7d367899
Jeff Sikala <sikala9@hotmail.com>
2018-10-18 16:59:59 -0400

    added the return statement to ec2_check custom grain

--- base/_grains/ec2_check.py
+++ base/_grains/ec2_check.py
@@ -23,6 +23,7 @@ def ec2_check():
     # After the grains ['cloud_info'] part, the [0] calls the first key/value pair in the append method array. In this case it's the provider = Amazon
     grains['cloud_info'][0]['instance_id'] = instance_id
     grains['cloud_info'][0]['instance_type'] = instance_type
+    return grains
 
 if __name__== '__main__':
     ec2_check()
