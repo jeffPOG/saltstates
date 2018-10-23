b99e43afb0bbe13e1fbded68f89e31e0d0792200
Jeff Sikala <sikala9@hotmail.com>
2018-10-23 14:26:01 -0400

    testing ec2 check grain

--- base/_grains/ec2_check.py
+++ base/_grains/ec2_check.py
@@ -6,7 +6,7 @@ def ec2_check():
 
     # Instantiate grains dictionary
     grains = {}
- try:
+ #try:
     # Instantiate grains key cloud info
     grains['cloud_info'] = []
 
@@ -23,9 +23,10 @@ def ec2_check():
     # After the grains ['cloud_info'] part, the [0] calls the first key/value pair in the append method array. In this case it's the provider = Amazon
     grains['cloud_info'][0]['instance_id'] = instance_id
     grains['cloud_info'][0]['instance_type'] = instance_type
+    #print grains
     return grains
-    except urllib2.URLError:
-        return False
+    #except urllib2.URLError:
+        #return False
 
 if __name__== '__main__':
     ec2_check()
