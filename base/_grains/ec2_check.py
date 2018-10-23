d37017b72a00d7bcf10996d7b84cdbbd09b35403
Jeff Sikala <sikala9@hotmail.com>
2018-10-23 14:57:57 -0400

    test 01

--- base/_grains/ec2_check.py
+++ base/_grains/ec2_check.py
@@ -8,26 +8,26 @@ def ec2_check():
     grains = {}
 
  try:
-    # Instantiate grains key cloud info
-    grains['cloud_info'] = []
+     # Instantiate grains key cloud info
+     grains['cloud_info'] = []
 
-    # Sets the Base url for base
-    base_url = 'http://169.254.169.254/latest/meta-data/'
+     # Sets the Base url for base
+     base_url = 'http://169.254.169.254/latest/meta-data/'
 
-    instance_id = urllib2.urlopen(base_url + '/instance-id')
-    instance_type = urllib2.urlopen(base_url + 'instance-type')
-    instance_id = instance_id.read()
-    instance_type = instance_type.read()
+     instance_id = urllib2.urlopen(base_url + '/instance-id')
+     instance_type = urllib2.urlopen(base_url + 'instance-type')
+     instance_id = instance_id.read()
+     instance_type = instance_type.read()
 
-    grains['cloud_info'].append({'provider': 'Amazon'})
+     grains['cloud_info'].append({'provider': 'Amazon'})
 
-    # After the grains ['cloud_info'] part, the [0] calls the first key/value pair in the append method array. In this case it's the provider = Amazon
-    grains['cloud_info'][0]['instance_id'] = instance_id
-    grains['cloud_info'][0]['instance_type'] = instance_type
-    #print grains
-    return grains
-except urllib2.URLError:
-    return False
+     # After the grains ['cloud_info'] part, the [0] calls the first key/value pair in the append method array. In this case it's the provider = Amazon
+     grains['cloud_info'][0]['instance_id'] = instance_id
+     grains['cloud_info'][0]['instance_type'] = instance_type
+     #print grains
+     return grains
+  except urllib2.URLError:
+     return False
 
 if __name__== '__main__':
     ec2_check()
